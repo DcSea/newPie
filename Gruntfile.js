bc7e8226277b7b986607d9dfc64a255404fc7ea7
ducong <ducong@domob.cn>
2014-09-18 15:48:24 +0800

    it's a new world

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      options: {
+        banner: '/*!<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: ['public/js/main.min.js','public/js/json2.min.js'],
+        dest: 'public/js/grunt.min.js'
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/*!<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {
+          'public/css/grunt.min.css': ['public/css/main.css','public/css/dev.css']
+        }
+      }
+    },
+    htmlmin: {
+      index: {
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true,
+          banner: '/*!<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        files: {
+          'views/index.min.ejs': 'views/index.ejs'
+        }
+      }
+    },
+    jshint: {
+      // define the files to lint
+      files: ['gruntfile.js', 'public/js/dev.js'],
+      // configure JSHint (documented at http://www.jshint.com/docs/)
+      options: {
+        // more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+    watch:{
+      js:{
+        files:['public/js/dev.js'],
+        tasks:['jshint','uglify']
+      },
+      css:{
+        files:['public/css/dev.css'],
+        tasks:['cssmin']
+      },
+      html:{
+        files:['views/index.ejs'],
+        tasks:['htmlmin']
+      }
+    }
+  });
+
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['uglify','cssmin','htmlmin','watch']);
+
+};
